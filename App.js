07efba4d0c5056498fa28c716c81a216ca78ea1a
Nethravathi Guggulla <nethravathi.guggulla@jdplc.com>
2024-12-09 18:08:29 +0530

    IgnitingApp Changes

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-{/* <div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h1>
-    </div>
-</div> */}
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 const parent = React.createElement("div", {id : "parent"},
     [
         React.createElement("div", {id : "child"}, [
